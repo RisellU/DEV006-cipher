820488c6b4dd54adc7f445e1775c55350bdc7c61
RisellU <rcum22@gmail.com>
2023-03-21 11:22:54 -0500

    mi punto de partida

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
-const cipher = {
-  // ...
-};
+//const cipher = {
+//crear funciones para hacer el cifrado
 
-export default cipher;
+ function cifrar(texto,desplazamiento){
+  let resultado = "";
+    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    desplazamiento = (desplazamiento % 26);
+    if (texto){
+      for (let i = 0; i<texto.length; i++){
+        if (letras.indexOf(texto[i])!=-1){
+          let posicion = ((letras.indexOf(texto[i])+desplazamiento)%26);
+          resultado += letras[posicion];
+        }
+        else {
+          resultado += texto[i];
+      }
+    }
+ }
+ return resultado;  
+}
+  //la variable posicion se creo para hacer el calculo del desplazamiento
+
+//};
+
+//export default cipher
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import cipher from './cipher.js';
+//no es para llamar un elemento del html. Se utiliza para escuchar 
+//cualquier evento que le pase al navegador con "load"
+window.addEventListener("load", inicio, true)
+//admitir en el campo de texto solo letras en mayúscula
+//usamos el evento keyUpp para evitar las letras en minuscula
+function inicio(){
+  document.getElementById("comentario").addEventListener("keyup", function(){
+    this.value = this.value.toUpperCase()
+  }, true); 
 
-console.log(cipher);
+//this.value llama el evento de "comentario"
+//this.value.toUpperCase sera igual al comentario escrito pero en mayuscula
+
+  document.getElementById("cifrar").addEventListener("click", function(){
+    let texto = document.getElementById("comentario").value
+    let desplazamiento = document.getElementById("desplazamiento").value
+    console.log(texto)
+    //document.getElementById("comentario2").value = cifrar(texto,desplazamiento)
+  }, true);
+//boton cifrar- el cual tomara ese mensaje escrito y para eso se le agrega .value
+//Y la funcion de desplazamiento para que se devuelva el texo a la segunda caja de texto
+//las variables texto y desplazamiento 
+}
